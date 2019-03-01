04ecde2c3cd563a45c7ef880e0e486b85336ba6b
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-01 11:13:58 -0700

    Register component set up

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, NavLink } from 'react-router-dom';
 
 import './App.css';
 import Login from './LoginComponents/Login';
+import Register from './RegisterComponents/Register';
 import Jokes from './JokesComponents/Jokes';
 
 class App extends Component {
@@ -13,10 +14,13 @@ class App extends Component {
           <nav>
             <NavLink to="/login">Log In</NavLink>
             &nbsp;|&nbsp;
+            <NavLink to="/register">Register</NavLink>
+            &nbsp;|&nbsp;
           </nav>
         </header>
         <main>
           <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
           <Route path="/jokes" component={Jokes} />
         </main>
       </div>
